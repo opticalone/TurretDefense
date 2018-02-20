a58ae6d57d0759343a293f0465fe30d1df359909
Cameron Zach P Tucker <s179071@students.aie.edu.au>
2018-02-20 14:44:43 -0800

    working on player movement and camera controls

---
+++ Assets/Zach/Scripts/CameraFollow.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+    public Camera cam;
+
+    public LayerMask collectibleLayer;
+
+    public float offset;         //Private variable to store the offset distance between the player and camera
+    public Vector3 mouseRotation = new Vector3(45, 90, 0);
+
+    public float rotationSpeed;
+    public bool keyboardOnly;
+    public float xAngle;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //float rotX = Input.GetAxis("CameraX");
+        //float rotY = Input.GetAxis("CameraY");
+        //Debug.Log(rotX.ToString() + " " + rotY.ToString());
+        //transform.Rotate(rotY, rotX, 0);
+        //transform.position = player.transform.position + offset;
+
+        if (!keyboardOnly)
+        {
+            mouseRotation.x -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            mouseRotation.y += Input.GetAxis("Mouse X") * rotationSpeed;
+        }
+        else if (keyboardOnly)
+        {
+            mouseRotation.x = xAngle;
+            mouseRotation.y += Input.GetAxis("Horizontal") * rotationSpeed;
+        }
+        
+        if (mouseRotation.x > 44)
+        {
+            mouseRotation.x = 44;
+        }
+        if (mouseRotation.x < -44)
+        {
+            mouseRotation.x = -44;
+        }
+
+        this.transform.position = player.transform.position;
+        this.transform.rotation = Quaternion.Euler(mouseRotation.x * 1.7f, mouseRotation.y * 1.7f, 0);
+        this.transform.position -= transform.forward * offset;
+
+        RaycastHit hitInfo;
+
+        Debug.DrawLine(player.transform.position, transform.position, Color.green);
+
+        Physics.Linecast(player.transform.position, transform.position, out hitInfo, ~collectibleLayer);
+        if (hitInfo.transform != null)
+        {
+            Debug.Log("hit");
+            transform.position = player.transform.position + (transform.forward * -(hitInfo.distance - cam.nearClipPlane));
+            Debug.DrawLine(player.transform.position, transform.position, Color.red);
+        }
+
+        //this.transform.position -= transform.forward * offset;
+
+
+
+
+    }
+}
---
+++ Assets/Zach/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float horizontal;
+    float vertical;
+    Rigidbody rb;
+
+    public Camera cam;
+    public float movementSpeed;
+    public float maxSpeed;
+
+    private float yAxisMovement;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        //find movement direction
+        float yAxisMovement = rb.velocity.y;
+        Vector3 targetDirection = new Vector3(horizontal, 0.0f, vertical);
+        targetDirection = cam.transform.TransformDirection(targetDirection);
+        targetDirection.y = 0f;
+
+        //find direction the chracter should be facing
+        Vector3 relativePos = -(cam.transform.position - transform.position);
+        relativePos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = rotation;
+
+        //movement
+        if (horizontal != 0 || vertical != 0)
+        {
+            rb.AddForce(targetDirection * movementSpeed);
+        }
+    }
+}
