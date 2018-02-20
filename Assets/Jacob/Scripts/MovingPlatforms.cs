8d290d345cb9abe826212358efffcee3db517134
Jacob M Miller <s179063@students.aie.edu.au>
2018-02-20 14:58:29 -0800

    Added Moving Platformes and removed worthless leftover scripts

---
+++ Assets/Jacob/Scripts/MovingPlatforms.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour {
+
+    public Transform[] Waypoints;
+    public float speed = 2;
+
+    public int CurrentPoint = 0;
+
+    void Update()
+    {
+        if (transform.position != Waypoints[CurrentPoint].transform.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, speed * Time.deltaTime);
+        }
+
+        if (transform.position == Waypoints[CurrentPoint].transform.position)
+        {
+            CurrentPoint += 1;
+        }
+        
+        if (CurrentPoint >= Waypoints.Length)
+        {
+            CurrentPoint = 0;
+        }
+    }
+}
